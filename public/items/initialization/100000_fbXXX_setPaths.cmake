9a81ac289547f189fdeadeb2083f09b1618ea257
Khalyutin <khalyutin@notesoft.net>
2023-05-04 19:04:42 +0300

    -

--- public/items/initialization/100000_fbXXX_setPaths.cmake
+++ public/items/initialization/100000_fbXXX_setPaths.cmake
@@ -1,12 +1,11 @@
 # pacman -S btyacc
 # pacman -S sed
 # // pacman -S mingw-w64-x86_64-libiconv
-# pacman -S mingw-w64-x86_64-iconv
-# pacman -S mingw-w64-clang-x86_64-libiconv
-# pacman -S libiconv-devel
+# // pacman -S mingw-w64-x86_64-iconv
+# // pacman -S mingw-w64-clang-x86_64-libiconv
+# // pacman -S libiconv-devel
 
-# https://stackoverflow.com/questions/68676184/clang-cl-error-lld-link-error-undefined-symbol-divti3
-
+#set(CMAKE_BINARY_DIR  ${CMAKE_SOURCE_DIR}/native/build)
 
 
 
